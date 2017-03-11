ca684cd7d855cc72eed01ff88ce287fcc5ca59ed
Rudie Dirkx <github@hotblocks.nl>
2017-03-11 20:19:41 +0100

    +interactive

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 
+const Title = ({color = 'red', children}) => <h1 style={{ color }}>{children}</h1>
+
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			title: '',
+			length: 0,
+			color: 'green'
+		};
+
+		this.updateText = this.updateText.bind(this);
+		this.updateColor = this.updateColor.bind(this);
+	}
+
+	updateText(e) {
+		this.setState({
+			title: e.target.value,
+			length: e.target.value.length,
+		});
+	}
+
+	updateColor(e) {
+		this.setState({
+			color: e.target.value
+		});
+	}
+
 	render() {
 		return (
 			<div style={{textAlign: 'center'}}>
-				<h1>Hello World!</h1>
+				<Title color={this.state.color}>Title is {this.state.length} chars long</Title>
+				<select onChange={this.updateColor}><option>green</option><option>red</option><option>blue</option></select>
+				<input onChange={this.updateText} />
 			</div>
 		);
 	}
