5439a20a7faf056f2c227ea931cab66ff07a0eef
Rudie Dirkx <github@hotblocks.nl>
2017-03-26 19:02:13 +0200

    Series is Redux

--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Demo from './components/Demo.jsx';
 import BR from './components/BR.mobx.jsx';
-import Series from './components/Series.jsx';
+import Series from './components/Series.redux.jsx';
 import { Router, Route, Redirect, Switch } from 'react-router';
 import { Link } from 'react-router-dom';
 import { createHashHistory } from 'history';
